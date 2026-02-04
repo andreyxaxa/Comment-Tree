f61387955a95d04d596eb70910717f7654aa03c7
Petrov Andrey <4ndrew1337@mail.ru>
2026-02-04 18:29:54 +0300

    feat(config): add config pkg (parse .env)

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type (
+	Config struct {
+		HTTP HTTP
+		Log  Log
+		PG   PG
+	}
+
+	HTTP struct {
+		Port string `env:"HTTP_PORT,required"`
+	}
+
+	Log struct {
+		Level string `env:"LOG_LEVEL,required"`
+	}
+
+	PG struct {
+		PoolMax int    `env:"PG_POOL_MAX,required"`
+		URL     string `env:"PG_URL,required"`
+	}
+)
+
+func New() (*Config, error) {
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	return cfg, nil
+}
