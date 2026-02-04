ff3c4067b04584eb4fb21a6f99e4e6842278e0eb
Petrov Andrey <4ndrew1337@mail.ru>
2026-02-05 02:30:17 +0300

    docs: add swagger documentation

--- internal/controller/restapi/router.go
+++ internal/controller/restapi/router.go
@@ -1,12 +1,17 @@
 package restapi
 
 import (
+	_ "github.com/andreyxaxa/Comment-Tree/docs" // Swagger docs.
 	v1 "github.com/andreyxaxa/Comment-Tree/internal/controller/restapi/v1"
 	"github.com/andreyxaxa/Comment-Tree/internal/usecase"
 	"github.com/andreyxaxa/Comment-Tree/pkg/logger"
 	"github.com/gofiber/fiber/v2"
 )
 
+// @title Comment tree
+// @version 1.0.0
+// @host localhost:8080
+// @BasePath /v1
 func NewRouter(app *fiber.App, c usecase.CommentUseCase, l logger.Interface) {
 	apiV1Group := app.Group("/v1")
 	{
--- internal/controller/restapi/v1/comment.go
+++ internal/controller/restapi/v1/comment.go
@@ -13,6 +13,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// @Summary Create new comment
+// @Description Creates new comment
+// @Tags comments
+// @Accept json
+// @Produce json
+// @Param request body request.CreateCommentRequest true "Comment"
+// @Success 201 {object} response.CreateCommentResponse
+// @Failure 400 {object} response.Error
+// @Failure 404 {object} response.Error
+// @Failure 500 {object} response.Error
+// @Router /v1/comments [post]
 func (r *V1) create(ctx *fiber.Ctx) error {
 	var body request.CreateCommentRequest
 
@@ -41,6 +52,20 @@ func (r *V1) create(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusCreated).JSON(resp)
 }
 
+// @Summary Get comments
+// @Description Get comment(s) with all replies, search, sort
+// @Tags comments
+// @Produce json
+// @Param parent_id query string false "Parent ID"
+// @Param search query string false "Search text"
+// @Param sort_by query string false "Sort option" Enums(created_at, id)
+// @Param order query string false "Sort order" Enums(asc, ASC, desc, DESC)
+// @Param limit query string false "Limit of comments on one page, default 20"
+// @Param offset query string false "Offset for displaying a specific page, default 0"
+// @Success 200 {object} response.PaginatedCommentsResponse
+// @Failure 400 {object} response.Error
+// @Failure 500 {object} response.Error
+// @Router /v1/comments [get]
 func (r *V1) getComments(ctx *fiber.Ctx) error {
 	var req request.GetCommentsReqeust
 
@@ -109,6 +134,15 @@ func (r *V1) getComments(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(resp)
 }
 
+// @Summary Delete comments
+// @Description Deletes comment by ID
+// @Tags comments
+// @Produce json
+// @Param id query string true "Comment ID"
+// @Success 204
+// @Failure 400 {object} response.Error
+// @Failure 404 {object} response.Error
+// @Failure 500 {object} response.Error
 func (r *V1) deleteCommentTree(ctx *fiber.Ctx) error {
 	id, err := ctx.ParamsInt("id")
 	if err != nil {
