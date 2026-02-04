5f25195a9cdd42cab30ef964068b4968cf076fcb
Petrov Andrey <4ndrew1337@mail.ru>
2026-02-04 18:34:11 +0300

    feat(core): add app - all components are created and run here

---
+++ internal/app/app.go
@@ -0,0 +1,57 @@
+package app
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/andreyxaxa/Comment-Tree/config"
+	"github.com/andreyxaxa/Comment-Tree/internal/controller/restapi"
+	"github.com/andreyxaxa/Comment-Tree/internal/repo/persistent"
+	"github.com/andreyxaxa/Comment-Tree/internal/usecase/comment"
+	"github.com/andreyxaxa/Comment-Tree/pkg/httpserver"
+	"github.com/andreyxaxa/Comment-Tree/pkg/logger"
+	"github.com/andreyxaxa/Comment-Tree/pkg/postgres"
+)
+
+func Run(cfg *config.Config) {
+	// Logger
+	l := logger.New(cfg.Log.Level)
+
+	// Repository
+	pg, err := postgres.New(cfg.PG.URL, postgres.MaxPoolSize(cfg.PG.PoolMax))
+	if err != nil {
+		l.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
+	}
+	defer pg.Close()
+
+	// Use-Case
+	commentUseCase := comment.New(
+		persistent.New(pg),
+	)
+
+	// HTTP Server
+	httpServer := httpserver.New(l, httpserver.Port(cfg.HTTP.Port))
+	restapi.NewRouter(httpServer.App, commentUseCase, l)
+
+	// Start Server
+	httpServer.Start()
+
+	// Waiting Signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		l.Info("app - Run - signal: %s", s.String())
+	case err = <-httpServer.Notify():
+		l.Error(fmt.Errorf("app - Run - httpServer.Notify: %v", err))
+	}
+
+	// Shutdown
+	err = httpServer.Shutdown()
+	if err != nil {
+		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %v", err))
+	}
+}
